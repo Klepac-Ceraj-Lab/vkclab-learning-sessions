e4ec8a463a6e015afdd5f8ca4593dedbd61cd4fd
gahanss-dev <gahan.sabbir@gmail.com>
2025-10-06 12:28:59 -0400

    Regressions

--- sessions/PLS.r
+++ sessions/PLS.r
@@ -16,7 +16,6 @@ invisible(lapply(packages, function(pkg) {
   suppressPackageStartupMessages(library(pkg, character.only = TRUE))
 }))
 
-
 #| code-fold: false
 dat <- data.frame(
     "MGX_Shannon_Index"     = c(2.5, 0.5, 2.2, 1.9, 3.1, 2.3, 2.0, 1.0),
@@ -31,29 +30,113 @@ dat
 X <- dat[, c("MGX_Shannon_Index", "MGX_InvSimpson_Index", "MBX_FreeIron", "MBX_Ferritin")]
 y <- dat[, "AnemiaRiskScore"]
 
-summary(X1)
-
+#Building PLS Model 
 pls_model <- plsr(AnemiaRiskScore~MGX_Shannon_Index+MGX_InvSimpson_Index+MBX_FreeIron+MBX_Ferritin, data = dat, scale = TRUE, validation = "LOO"); summary(pls_model)
 summary(pls_model)
-predict_pls <- predict(pls_model, ncomp = pls_model$ncomp)
+predict_pls <- predict(pls_model, ncomp = 1)
+predict_pls_2 <- predict(pls_model, ncomp = 2)
 observed_pls <- y
-pls_plot_data <- cbind(y, predict_pls)
+pls_plot_data <- as.data.frame(cbind(y, predict_pls))
+pls_plot_data_2 <- as.data.frame(cbind(y, predict_pls_2))
 colnames(pls_plot_data) <- c("Observed", "Predicted")
-pls_plot_data <- as.data.frame(pls_plot_data)
+colnames(pls_plot_data_2) <- c("Observed", "Predicted")
 
-pls_graph <- ggplot(pls_plot_data, aes(x=Observed, y=Predicted)) + 
+
+#PLS prediction with ncomp = 1
+pls_graph_1 <- ggplot(pls_plot_data, aes(x=Observed, y=Predicted)) + 
 geom_point(size = 3) +
 stat_cor(method = "pearson", label.x = 6, label.y = 4.4) +
   geom_smooth(method = "lm", color = "red", se = FALSE) +
   theme_minimal() +
   labs(
-    title = "Predicted vs Observed Anemia Risk Score",
+    title = "Predicted vs Observed Anemia Risk Score, ncomp = 1",
     x = "Observed Anemia Risk Score",
     y = "Predicted Anemia Risk Score"
   )
 
-pls_graph
+pls_graph_1
 
-ggsave("pls_graph.png", width = 6, height =4, dpi = 300)
+ggsave("pls_graph_1.png", plot="pls_graph_1", width = 6, height =4, dpi = 300)
+
+#PLS prediction with ncomp = 2
+pls_graph_2 <- ggplot(pls_plot_data_2, aes(x=Observed, y=Predicted)) + 
+geom_point(size = 3) +
+stat_cor(method = "pearson", label.x = 6, label.y = 4.4) +
+  geom_smooth(method = "lm", color = "red", se = FALSE) +
+  theme_minimal() +
+  labs(
+    title = "Predicted vs Observed Anemia Risk Score, ncomp = 2",
+    x = "Observed Anemia Risk Score",
+    y = "Predicted Anemia Risk Score"
+  )
+
+pls_graph_2
+
+ggsave("pls_graph_2.png", plot = "pls_graph_2" width = 6, height =4, dpi = 300)
 
 pls_loadings <- data.frame(pls_model$loading.weights[,1:2], var = rownames(pls_model$loading.weights))
+
+# PCR
+X_scaled <- scale(X)
+dat_scaled <- data.frame(X_scaled, y)
+# run pcr with cross validation to see how many PCs to keep
+pcr_model <- pcr(y ~ ., data = dat_scaled, scale =TRUE, validation = "LOO")
+#print(summary(pcr_model))
+print("pcr loadings: ")
+pcr_loadings <- loadings(pcr_model)
+print(pcr_loadings)
+# mean squared error of prediction
+validationplot(pcr_model, val.type = "MSEP")
+
+#PCR Prediction using ncomp = 1
+predicted_y <- as.vector(predict(pcr_model, ncomp=1))
+results <- data.frame(
+  Observed = y,
+  Predicted = predicted_y
+)
+print(results)
+ggplot(results, aes(x = Observed, y = Predicted)) +
+  geom_point(size = 3, color = "blue") +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "gray") +
+  labs(
+    title = "PCR of Observed vs Predicted Anemia Risk, ncomp = 1",
+    x = "Observed Anemia Risk Score",
+    y = "Predicted Anemia Risk Score"
+  ) +
+  theme_minimal(base_size = 14)
+# Save it
+ggsave("pcr_1.png", plot = "results", width = 6, height = 4, dpi = 300)
+
+#PCR Prediction using ncomp = 2
+predicted_y_2 <- as.vector(predict(pcr_model, ncomp=2))
+results_2 <- data.frame(
+  Observed = y,
+  Predicted = predicted_y_2
+)
+print(results_2)
+ggplot(results_2, aes(x = Observed, y = Predicted)) +
+  geom_point(size = 3, color = "blue") +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "gray") +
+  labs(
+    title = "PCR of Observed vs Predicted Anemia Risk, ncomp = 2",
+    x = "Observed Anemia Risk Score",
+    y = "Predicted Anemia Risk Score"
+  ) +
+  theme_minimal(base_size = 14)
+# Save it
+ggsave("pcr_2.png", plot = "results_2" width = 6, height = 4, dpi = 300)
+
+#Linear Regression for Ferriting and Free Iron against Anemia Risk 
+Ferritin_Regression <- ggplot(dat,aes(MBX_Ferritin, AnemiaRiskScore)) + 
+stat_summary(fun.data=mean_cl_normal) + 
+  geom_smooth(method='lm', formula= y~x)
+ggsave("Ferritin_Regression.png", plot = Ferritin_Regression, width = 6, height = 4, dpi = 300)
+
+Iron_Regression <- ggplot(dat,aes(MBX_FreeIron, AnemiaRiskScore)) +
+stat_summary(fun.data=mean_cl_normal) + 
+  geom_smooth(method='lm', formula= y~x)
+ggsave("Iron_Regression.png", plot = Iron_Regression, width = 6, height = 4, dpi = 300)
+
+
+FerritionIronCor <- cor(dat$MBX_Ferritin, dat$MBX_FreeIron)
+
